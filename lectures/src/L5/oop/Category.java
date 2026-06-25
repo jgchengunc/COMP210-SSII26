00d15038ef142bdae306965b9d5d77bf53453fc8
jgchengunc <jgcheng@unc.edu>
2026-06-25 18:45:31 -0400

    Add non-OOP example

---
+++ lectures/src/L5/oop/Category.java
@@ -0,0 +1,43 @@
+package L5.oop;
+
+// Initial Non-OOP example
+public class Category {
+    public static void main(String[] args) {
+        double pointAX = 0.0;
+        double pointAY = 0.0;
+        double pointBX = 2.0;
+        double pointBY = 3.0;
+        double pointCX = 4.0;
+        double pointCY = 0.0;
+
+        String category = triangleCategory(pointAX, pointAY, pointBX, pointBY, pointCX, pointCY);
+        System.out.println("Category: " + category);
+    }
+
+    public static String triangleCategory(double pointAX, double pointAY, double pointBX,
+                                          double pointBY, double pointCX, double pointCY) {
+        double sideABLength = pointDistance(pointAX, pointAY, pointBX, pointBY);
+        double sideBCLength = pointDistance(pointBX, pointBY, pointCX, pointCY);
+        double sideACLength = pointDistance(pointAX, pointAY, pointCX, pointCY);
+
+        if (closeEnough(sideABLength, sideBCLength) &&
+                closeEnough(sideBCLength, sideACLength)) {
+            return "equilateral";
+        } else if (closeEnough(sideABLength, sideBCLength) ||
+                closeEnough(sideBCLength, sideACLength) ||
+                closeEnough(sideABLength, sideACLength)) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+
+    private static boolean closeEnough(double side1Length, double side2Length) {
+        // Epsilon bound test for side length differences
+        return Math.abs(side1Length - side2Length) < 0.001;
+    }
+
+    private static double pointDistance(double point1X, double point1Y, double point2X, double point2Y) {
+        return Math.sqrt(Math.pow(point2X - point1X, 2) + Math.pow(point2Y - point1Y, 2));
+    }
+}
